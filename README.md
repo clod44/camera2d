95f5ec8d2750a820a06e8899b4ace79494bffdf9
clod44 <Rt_Sayochi@hotmail.com>
2023-06-06 15:29:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# camera2d
+ p5js Drag&Zoom camera along with basic hud for debugging values
---
+++ sketch.js
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+let canvasHolder = document.getElementById("canvas-holder");
+let canvas; 
+
+let camera2d;
+let debugWindow;
+
+let images = [];
+function preload(){
+    images = [  
+                loadImage("./assets/picture.jpg"),
+                loadImage("./assets/man.jpg")
+             ];
+}
+
+function setup(){
+    //init
+    canvas = createCanvas(100,100);
+    background(0,255,0);
+    canvas.parent("canvas-holder");
+    noSmooth();
+    resizeCanvas(canvasHolder.clientWidth, canvasHolder.clientHeight);
+
+    camera2d = new Camera2D();
+
+    camera2d.addDebugValue('camera2d', "");
+    camera2d.addDebugValue('FPS', () => frameRate().toFixed(2));
+    camera2d.addDebugValue('Cam X', () => camera2d.x.toFixed(2));
+    camera2d.addDebugValue('Cam Y', () => camera2d.y.toFixed(2));
+    camera2d.addDebugValue('Cam scale', () => camera2d.scale.toFixed(2));
+}
+
+
+function draw(){
+    background(0);
+
+
+    push();
+    camera2d.update();
+        
+
+    camera2d.showUnboundedImage(images[0]);
+    camera2d.showUnboundedImage(images[1], {
+        xOffset : width*0.5,
+        yOffset : height*0.5,
+        imageWidth : 200,
+        imageHeight : 200
+    });
+    
+    //image(vid,-50,-30,100,100);
+
+
+    camera2d.applyTransformations();
+    pop();
+
+    //grids and debug
+    camera2d.showDebugValues();
+    image(camera2d.graphics, 0, 0, width, height);
+
+
+
+
+}
+
+  
+function windowResized() {
+    resizeCanvas(canvasHolder.clientWidth, canvasHolder.clientHeight);
+    
+    camera2d.setResolution(width,height);
+}
+
+
+
+
+
+
+
